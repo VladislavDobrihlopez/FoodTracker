7a5848fbdb708fdab8efb7388b9e90070c96170b
Vladislav Voitov <dobrihlopez@gmail.com>
2023-10-18 01:08:32 +0300

    bug fix unexpected crash

--- tracker/tracker_presentation/src/main/java/com/voitov/tracker_presentation/health_tracker_screen/model/TimePointResult.kt
+++ tracker/tracker_presentation/src/main/java/com/voitov/tracker_presentation/health_tracker_screen/model/TimePointResult.kt
@@ -1,11 +1,14 @@
 package com.voitov.tracker_presentation.health_tracker_screen.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.time.LocalDate
 
+@Parcelize
 data class TimePointResult(
     val date: LocalDate,
     val inTotalKkal: Int,
     val kkalInFats: Int,
     val kkalInCarbs: Int,
     val kkalInProteins: Int
-)
+): Parcelable
